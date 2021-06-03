0df02e0e6817f6fc3603da6c97b28ee51ec9e850
mcepeda <mcepeda@intergrupo.com>
2021-06-03 10:18:49 -0500

    correccion de configuracion de index.js backend

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
+const path = require('path');
 
 // crear el servidor de express
 const app = express();
@@ -22,6 +23,10 @@ app.use('/api/hospitales', require('./routes/hospitales.routes'));
 app.use('/api/medicos', require('./routes/medicos.routes'));
 app.use('/api/search', require('./routes/search.routes'));
 app.use('/api/upload', require('./routes/upload.routes'));
+
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname,'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`corriendo en puerto ${process.env.PORT}`);
